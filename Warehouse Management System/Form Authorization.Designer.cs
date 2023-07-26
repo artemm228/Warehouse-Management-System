8bcd2832f7fb8102b97c31316bebe67f5aac114f
_ArtemSharaievskyi <87766261+artemm228@users.noreply.github.com>
2023-07-26 15:45:34 +0200

    Add files via upload

---
+++ Warehouse Management System/Form Authorization.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Warehouse_Management_System
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.cbshow_pass = new System.Windows.Forms.CheckBox();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.tbusername = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbclose = new System.Windows.Forms.Label();
+            this.tbpassword = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.tbpassword);
+            this.panel1.Controls.Add(this.cbshow_pass);
+            this.panel1.Controls.Add(this.btnlogin);
+            this.panel1.Controls.Add(this.tbusername);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(-1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(446, 440);
+            this.panel1.TabIndex = 0;
+            // 
+            // cbshow_pass
+            // 
+            this.cbshow_pass.AutoSize = true;
+            this.cbshow_pass.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cbshow_pass.Location = new System.Drawing.Point(107, 344);
+            this.cbshow_pass.Name = "cbshow_pass";
+            this.cbshow_pass.Size = new System.Drawing.Size(108, 18);
+            this.cbshow_pass.TabIndex = 5;
+            this.cbshow_pass.Text = "Show password";
+            this.cbshow_pass.UseVisualStyleBackColor = true;
+            this.cbshow_pass.CheckedChanged += new System.EventHandler(this.cbshow_pass_CheckedChanged);
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnlogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnlogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnlogin.Location = new System.Drawing.Point(104, 374);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(213, 36);
+            this.btnlogin.TabIndex = 4;
+            this.btnlogin.Text = "Log In";
+            this.btnlogin.UseVisualStyleBackColor = false;
+            this.btnlogin.Click += new System.EventHandler(this.btnlogin_Click);
+            // 
+            // tbusername
+            // 
+            this.tbusername.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbusername.Location = new System.Drawing.Point(104, 259);
+            this.tbusername.Multiline = true;
+            this.tbusername.Name = "tbusername";
+            this.tbusername.Size = new System.Drawing.Size(213, 30);
+            this.tbusername.TabIndex = 2;
+            this.tbusername.Enter += new System.EventHandler(this.tbusername_Enter);
+            this.tbusername.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbusername_KeyPress);
+            this.tbusername.Leave += new System.EventHandler(this.tbusername_Leave);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Warehouse_Management_System.Properties.Resources.login;
+            this.pictureBox1.Location = new System.Drawing.Point(104, 48);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(213, 205);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.lbclose);
+            this.panel2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(445, 40);
+            this.panel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(139, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Login Form";
+            // 
+            // lbclose
+            // 
+            this.lbclose.AutoSize = true;
+            this.lbclose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lbclose.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbclose.Location = new System.Drawing.Point(418, -1);
+            this.lbclose.Name = "lbclose";
+            this.lbclose.Size = new System.Drawing.Size(27, 25);
+            this.lbclose.TabIndex = 0;
+            this.lbclose.Text = "X";
+            this.lbclose.Click += new System.EventHandler(this.lbclose_Click);
+            // 
+            // tbpassword
+            // 
+            this.tbpassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbpassword.Location = new System.Drawing.Point(104, 308);
+            this.tbpassword.Multiline = true;
+            this.tbpassword.Name = "tbpassword";
+            this.tbpassword.Size = new System.Drawing.Size(213, 30);
+            this.tbpassword.TabIndex = 6;
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 437);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormLogin";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lbclose;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnlogin;
+        private System.Windows.Forms.TextBox tbusername;
+        private System.Windows.Forms.CheckBox cbshow_pass;
+        private System.Windows.Forms.TextBox tbpassword;
+    }
+}
+
---
+++ Warehouse Management System/Form Authorization.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Warehouse_Management_System
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+
+            tbusername.Text = "username";
+            tbusername.ForeColor = SystemColors.GrayText;
+
+            tbusername.Enter += tbusername_Enter;
+            tbusername.Leave += tbusername_Leave;
+
+            tbpassword.Text = "password";
+            tbpassword.ForeColor = SystemColors.GrayText;
+
+            tbpassword.Enter += tbpassword_Enter;
+            tbpassword.Leave += tbpassword_Leave;
+
+
+            btnlogin.FlatStyle = FlatStyle.Flat; // Set button style to flat
+            btnlogin.FlatAppearance.BorderColor = Color.Black; // Set the desired border color
+            btnlogin.FlatAppearance.BorderSize = 0;
+
+
+            this.StartPosition = FormStartPosition.CenterScreen; // to center this form.
+                                                                 // the form will be displayed in the center of the screen when it launches.
+        }
+        private void tbusername_Enter(object sender, EventArgs e)
+        {
+            if (tbusername.Text == "username")
+            {
+                tbusername.Text = "";
+                tbusername.ForeColor = SystemColors.WindowText;
+            }
+        }
+        private void tbusername_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tbusername.Text))
+            {
+                tbusername.Text = "username";
+                tbusername.ForeColor = SystemColors.GrayText;
+            }
+        }
+
+        private void lbclose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnlogin_Click(object sender, EventArgs e)
+        {
+            if (tbusername.ForeColor == SystemColors.GrayText && tbpassword.ForeColor == SystemColors.GrayText)
+                MessageBox.Show("Error. Enter username and password");
+            else if (tbpassword.ForeColor == SystemColors.GrayText)
+                MessageBox.Show("Error. Enter password");
+            else if (tbusername.ForeColor == SystemColors.GrayText)
+                MessageBox.Show("Error. Enter username");
+            else
+            {
+                // transition to a functional form
+            }
+
+            
+        }
+
+        private void tbusername_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == ' ')
+                e.Handled = true;
+        }
+
+        private void tbpassword_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tbpassword.Text))
+            {
+                tbpassword.Text = "password";
+                tbpassword.ForeColor = SystemColors.GrayText;
+            }
+        }
+
+        private void tbpassword_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == ' ')
+                e.Handled = true;
+        }
+
+        private void tbpassword_Enter(object sender, EventArgs e)
+        {
+            if (tbpassword.Text == "password")
+            {
+                tbpassword.Text = "";
+                tbpassword.ForeColor = SystemColors.WindowText;
+            }
+        }
+
+        private void cbshow_pass_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbshow_pass.Checked)
+            {
+                tbpassword.PasswordChar = '*';
+            }
+            else
+            {
+                tbpassword.PasswordChar = '\0';
+            }
+        }
+    }
+}
